34de7a21b3e24ff0e6bb841a9dbdad7397fb4632
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-27 15:57:02 -0500

    added app.js file and added dependencies and set up express app variables within file

---
+++ Develop/public/public/js/app.js
@@ -0,0 +1,12 @@
+// Dependencies
+
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
