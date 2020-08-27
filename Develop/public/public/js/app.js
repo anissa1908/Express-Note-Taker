0bdb8aff2648e1ab2d0ccdf72e9387108bd696d7
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-27 16:48:14 -0500

    added html routes and get post and delete coding

--- Develop/public/public/js/app.js
+++ Develop/public/public/js/app.js
@@ -3,10 +3,41 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const { Console } = require("console");
 
 
 // Sets up the Express App
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// HTML ROUTES
+app.get('./index.html', (req, res) =>{
+    res.send('Does this thing work?');
+});
+
+
+
+// GET
+
+app.get ('/api/notes', function(req, res) {
+    res.send (`db.json`)
+});
+
+app.get (`*`, function (req,res) {
+    res.send (`index.html`)
+}
+
+// POST
+
+app.post (`/api/notes`, function(req, res) {}
+
+});
+
+// DELETE
+
+app.delete ('/', function(req, res) {
+
+});
